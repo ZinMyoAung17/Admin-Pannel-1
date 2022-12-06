590532908e64b874703ee68d1cd957e13bdad86e
ZinMyoAung17 <Maaloeleelar56@gmail.com>
2022-12-07 03:35:43 +0630

    first commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+let sidemenu = document.querySelector("aside") ; 
+let menuBtn = document.querySelector("#menu-btn") ; 
+let closeBtn = document.querySelector("#close-btn") ; 
+
+let themeToggler = document.querySelector(".theme-toggler")
+
+let container = document.querySelector("container")
+
+
+
+menuBtn.addEventListener('click', function(){
+
+    sidemenu.style.display = "block" ; 
+
+
+
+})
+
+
+closeBtn.addEventListener('click', function(){
+
+    sidemenu.style.display = "none" ; 
+    
+
+
+})
+
+
